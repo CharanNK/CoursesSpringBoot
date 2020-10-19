ac2291c3186c3081f2e14bf3047a678af0b493cd
CharanNK <charkeet@gmail.com>
2020-10-19 10:45:40 +0530

    bean missing exception

--- src/main/java/io/charan/springbootstarted/CourseApiApp.java
+++ src/main/java/io/charan/springbootstarted/CourseApiApp.java
@@ -1,8 +1,14 @@
 package io.charan.springbootstarted;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
 public class CourseApiApp {
 
--- src/main/java/io/charan/springbootstarted/topicjpa/TopicJPAService.java
+++ src/main/java/io/charan/springbootstarted/topicjpa/TopicJPAService.java
@@ -1,8 +1,11 @@
 package io.charan.springbootstarted.topicjpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.*;
 
+@Service
 public class TopicJPAService {
 
 	@Autowired
